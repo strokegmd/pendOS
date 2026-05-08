bf959c20941074815da8834b27202fb8ab5a0f03
strokegdz <154758467+strokegmd@users.noreply.github.com>
2026-05-08 18:01:38 +0300

    unfinished disk2arr tool

---
+++ tools/disk2arr.py
@@ -0,0 +1,48 @@
+import struct
+import os
+
+class ByteStream:
+    def __init__(self: 'ByteStream') -> None:
+        self.bytes = b''
+    
+    def write_byte(self: 'ByteStream', value: int | bool) -> None:
+        self.bytes += struct.pack('B', value)
+    
+    def write_bytes(self: 'ByteStream', bytes: bytes) -> None:
+        self.bytes += bytes
+
+class Entry:
+    def __init__(self: 'Entry', is_directory: bool, path: str, content: bytes) -> None:
+        self.is_directory = is_directory
+        self.path = path
+        self.content = content
+    
+    def serialize(self: 'Entry') -> bytes:
+        buffer = ByteStream()
+        buffer.write_byte(self.is_directory)
+        buffer.write_bytes(self.path.encode())
+        buffer.write_bytes(self.content)
+        
+        return buffer.bytes
+
+def main() -> None:
+    disk = ByteStream()
+    for path in os.listdir('../disk'):
+        if '.' not in path:
+            continue # TODO: directories are currently unsupported
+
+        with open(f'../disk/{path}', 'rb') as file:
+            entry = Entry(False, f'/{path}', file.read())
+            disk.write_bytes(entry.serialize())
+    
+    array = f'uint8_t *disk = ['
+    for byte in disk.bytes:
+        array += f'{hex(byte)}, '
+    
+    array = array[:-2]
+    array += '];'
+
+    print(array)
+
+if __name__ == '__main__':
+    main()
